d2b77e92e11088edf3940310df228f079127b7e0
Cindy Cortes <cindy.indins@gmail.com>
2019-02-13 17:35:33 -0700

    Recipe list

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { recipes } from './tempList';
+import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
 
 class App extends Component {
+  state = {
+    recipes: recipes,
+    url:
+      "https://www.food2fork.com/api/search?key=8ae4df7876abd9f20401cfdb2a44c75e&q",
+    details_id: 35375
+  };
+
+
+  async getRecipes() {
+    try {
+      const data = await fetch(this.state.url); // first get the data
+      const jsonData = await data.json(); // json data
+      this.setState({
+        recipes: jsonData.recipes 
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  componentDidMount() {
+    this.getRecipes()
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    // console.log(this.state.recipes);
+
+    return <React.Fragment>
+
+      <RecipeList recipes={this.state.recipes} />
+      <RecipeDetails id={this.state.details_id} />
+    </React.Fragment>
+
+
   }
 }
 
---
+++ src/components/RecipeSearch.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Recipe from './Recipe';
+
+class RecipeSearch extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello From Search </h1>
+
+            </div>
+        )
+
+
+
+    }
+}
+export default RecipeSearch
